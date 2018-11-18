cc3bc6aef4493ff6786e1c66194c9439cb908c6d
Artem Konyshev <konyshev_arte@mail.ru>
2018-11-18 09:57:12 +0500

    added equal and not equals operators for Vec

--- src/Models/Vec.cs
+++ src/Models/Vec.cs
@@ -1,15 +1,17 @@
 namespace thegame.Models
 {
-	public class Vec
-	{
-		public Vec(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
+    public class Vec
+    {
+        public Vec(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
 
-		public readonly int X, Y;
+        public readonly int X, Y;
 
-		public static Vec operator +(Vec v1, Vec v2) => new Vec(v1.X+v2.X,v1.Y+v2.Y);
-	}
+        public static Vec operator +(Vec v1, Vec v2) => new Vec(v1.X + v2.X, v1.Y + v2.Y);
+        public static bool operator ==(Vec v1, Vec v2) => v1?.X == v2?.X && v1?.Y == v2?.Y;
+        public static bool operator !=(Vec v1, Vec v2) => !(v1 == v2);
+    }
 }
