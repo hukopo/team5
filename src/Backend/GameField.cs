8ac38aedda794e88ded81f4d6210222b669898cb
Max <lamax_99@mail.ru>
2019-02-23 13:06:22 +0500

    Add backend initial

---
+++ src/Backend/GameField.cs
@@ -0,0 +1,7 @@
+﻿namespace thegame.backend
+{
+    public class GameField
+    {
+        public readonly int[,] Field;
+    }
+}
---
+++ src/Backend/GamesKeeper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace thegame.backend
+{
+    public static class GamesKeeper
+    {
+        public static Guid CreateNewGame(int fieldSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GetStatus(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int[,] GetMap(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetScore(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int[,] MakeMove(Guid gameId, Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
