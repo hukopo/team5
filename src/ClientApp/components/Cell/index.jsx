38d3f8f4b574417a4ba325678af6f44bc38be5f1
hukopo <hukopo@yandex.ru>
2019-02-23 12:56:33 +0500

    0 -> null

--- src/ClientApp/components/Cell/index.jsx
+++ src/ClientApp/components/Cell/index.jsx
@@ -3,9 +3,24 @@ import styles from './styles.css'
 import PropTypes from 'prop-types'
 
 export default class Cell extends React.Component {
+    constructor(props) {
+        super(props)
+        let style;
+        switch (this.props.value) {
+            case 2:
+                style = styles.tile2
+                break;
+            case 4:
+                style = styles.tile4
+                break;
+        }
+//debugger;
+        this.className = styles.cell + " " + styles.tile + " " + styles.tile2
+
+    }
     render() {
         return (
-            <div className={styles.cell}>{this.props.value}</div>
+            <div className={this.className}>{this.props.value == 0 ? null : this.props.value}</div>
         );
     }
 }
