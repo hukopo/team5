672b0ed5efd874b39d4d5b1a3db4ad95b0025b03
hukopo <hukopo@yandex.ru>
2019-02-23 11:01:22 +0500

    get

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import styles from './styles.css'
 
 export default class Field extends React.Component {
-    render () {
+
+    componentDidMount = () => {
+        fetch("http://localhost:5000/api/game/map")
+            .then(response => {
+                response.json()
+                debugger;
+            })
+            .then(response => console.log(response));
+    }
+
+    render() {
         return (
-            <div className={ styles.root }/>
+            <div className={styles.root}>
+                sd;lfls;ldmf</div>
         );
     }
 }
