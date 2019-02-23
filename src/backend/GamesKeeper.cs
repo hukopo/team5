b81a1c922185b53c90b6c3e767b4b5d9a7fef808
Max <lamax_99@mail.ru>
2019-02-23 11:41:10 +0500

    Add facade GameKeeper.cs stub and Direction.cs

---
+++ src/backend/GamesKeeper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace thegame.backend
+{
+    public class GamesKeeper
+    {
+        public Guid CreateNewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetStatus(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[,] GetMap(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetScore(Guid gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[,] MakeMove(Guid gameId, Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
