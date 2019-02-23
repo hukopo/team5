7c5f9487dcab22c3cf52914eaded9c8657014027
devmentality <nikita.volnov.1999@mail.ru>
2019-02-23 14:59:50 +0500

    Field populator

---
+++ src/Backend/FieldPopulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using thegame.backend;
+
+namespace thegame.Backend
+{
+    public static class FieldPopulator
+    {
+        public static void Populate(GameField field)
+        {
+            var emptyCells = new List<Tuple<int, int>>();
+            for(var i = 0; i < field.Size; i++)
+                for(var j = 0; j < field.Size; j++)
+                    if (field.Field[i, j] == 0)
+                        emptyCells.Add(Tuple.Create(i, j));
+
+            var rnd = new Random();
+            var cellIndex = rnd.Next(0, emptyCells.Count - 1);
+            var cell = emptyCells[cellIndex];
+
+            var val = rnd.NextDouble();
+            field.Field[cell.Item1, cell.Item2] = val < 0.8 ? 2 : 4;
+        }
+    }
+}
